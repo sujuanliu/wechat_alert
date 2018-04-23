9ed5dffe240e0fa30c0d0db8da43fa9dca581924
sujuanliu <sjliu@bainainfo.com>
2018-04-23 16:29:33 +0800

    add readme files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # wechat_alert
+
+1. 运行前请先安装相应的模块
+ - pip3 install -r requirements
+2. 运行程序
+ - python3 runner.py
--- limao_runner.py
+++
@@ -1,8 +0,0 @@
-from config_setting import config
-from runner import *
-
-if __name__ == "__main__":
-	# run(config['es_setting']['limao_index_name'], 
-	# 	config['monitor_config']['limao_threhold_num'],
-	# 	config['wechat_config']['limao_secret'],
-	# 	config['wechat_config']['limao_agent_id'] )
